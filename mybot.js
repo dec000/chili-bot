46c75870f7147285ef881ceb5aee927b015aacd4
dec000 <34167457+dec000@users.noreply.github.com>
2017-12-01 11:37:53 -0600

    Update mybot.js

--- mybot.js
+++ mybot.js
@@ -9,6 +9,37 @@ client.on("message", (message) => {
   if (message.content.startsWith("chili!Hi")) {
     message.channel.send("Hello. :hot_pepper:");
   }
-});
+});  
 
+// !chili!dance
+case 'chili!dance':
+       bot.sendMessage({
+            to: channelID,
+            message: 'No way!'
+          });
+  break;
+
+      // chili!help
+      case 'ping':
+      bot.sendMessage({
+           to: channelID,
+           message: 'chili!dance, chili!Hi, chili!love, chili!heart'
+         });
+ break;
+
+     // chili!love
+     case 'ping':
+     bot.sendMessage({
+          to: channelID,
+          message: ':heart: :hot_pepper: :heart:'
+        });
+break;
+
+    // chili!heart
+    case 'ping':
+    bot.sendMessage({
+         to: channelID,
+         message: ':ChiliNut: + :Cappy: = :ChiliNut: :heart: :BananaNutCream:'
+       });
+break;
 client.login("Mzg2MTc1NTUzMjA5ODkyODc2.DQMLIw.5p_cWVH__rm_0dylRomEa-S1tf8");
